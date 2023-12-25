1ca1f8bddf0ad78d21c39dbf57b69fed6eb6160f
Abdallh Mahmood <a64mahmo@uwaterloo.ca>
2023-12-24 22:42:38 -0500

    Update

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 
-
+// Global variables
 let searchInput = document.getElementById('search-input');
 const searchResults = document.getElementById('search-results');
-let searchBtn = document.getElementById('search-btn');
 let emptySearch = document.getElementById('empy_state');
 const apikey = 'bc20f6e3';
 
-// create a new keyboard event and set the key to "Enter"
-const event = new KeyboardEvent('keydown', {
-    key: 'Enter',
-    code: 'Enter',
-    which: 13,
-    keyCode: 13,
-  });
-  
+// Event listener for search input
+searchInput.addEventListener('keyup', function (event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        // Your search logic here
+        const searchTerm = searchInput.value;
+        if (searchTerm) {
+            clearSearchInput();
+            handleSearch(searchTerm);
+            emptySearch.style.display = 'none';
+        }
+    }
+});
 
+// Function to handle search
 async function handleSearch(searchTerm) {
     const res = await fetch(`http://www.omdbapi.com/?apikey=${apikey}&s=${searchTerm}`);
     const movieList = await res.json();
-    
+
     let movieIDList = [];
 
-    movieList.Search.forEach(movie => {
-        movieIDList.push(movie.imdbID);
-    });
-    
-    handleSearchID(movieIDList);
-    clearSearchResults();
+    if (movieList.Response === 'False') {
+        searchResults.innerHTML = '<h1 class="no-results"> No results found </h1>';
+        console.log('No movies found');
+        return;
+    } else {
+        movieList.Search.forEach(movie => {
+            movieIDList.push(movie.imdbID);
+        });
+
+        handleSearchID(movieIDList);
+        clearSearchResults();
+    }
 }
 
+// Function to handle search by movie ID
 async function handleSearchID(movieIDList) {
     let movieList = [];
     for (const movieID of movieIDList) {
@@ -39,18 +54,22 @@ async function handleSearchID(movieIDList) {
     printMovies(movieList);
 }
 
+// Function to clear search results
 function clearSearchResults() {
     searchResults.innerHTML = '';
 }
 
+// Function to clear search input
 function clearSearchInput() {
     searchInput.value = '';
 }
 
+// Function to print movies
+
 function printMovies(movies) {
-    if(movies) {
+    if (movies) {
         movies.forEach(movie => {
-            searchResults.innerHTML += `             
+            searchResults.innerHTML += `
             <div class="border-wrap">
             <article class="movie_card ">
               <img
@@ -67,7 +86,7 @@ function printMovies(movies) {
                 <div class="card_action">
                   <p class="movie_runtime">${movie.Runtime}</p>
                   <p class="movie_genre">${movie.Genre}</p>
-                  <button class="add-btn">Add</button>
+                  <button class="add-btn" id="${movie.imdbID}">Add</button>
                 </div>
 
                 <p class="movie_plot">
@@ -76,35 +95,32 @@ function printMovies(movies) {
               </div>
             </article>
           </div>`;
+
+
         });
-    } else {
-        searchResults.innerHTML += '<h1 class="no-results"> No results found </h1>';
-        console.log('No movies found');
     }
 }
 
+// Add event listener to add button
+let savedMoviesIDs = [];
+searchResults.addEventListener('click', function(event) {
 
-// searchInput.dispatchEvent('click', e => {
-//     e.preventDefault();
-// });
-
-searchInput.addEventListener('keyup', function(event) {
-    
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-        // Cancel the default action, if needed
-        event.preventDefault();
-        // Trigger the button element with a click
-        // Your search logic here
-        const searchTerm = searchInput.value;
-        if (searchTerm) {
-            clearSearchInput();
-            handleSearch(searchTerm);
-            emptySearch.style.display = 'none';
-        
+    if (event.target.className === 'add-btn') {
+        const movieID = event.target.id;
+        if (savedMoviesIDs.includes(movieID)) {
+            return;
+        } else {
+            savedMoviesIDs.push(movieID);
+            localStorage.setItem('savedMoviesIDs', JSON.stringify(savedMoviesIDs));
+            console.log(savedMoviesIDs);
         }
     }
-  });
+});
 
-
-
+// Function to print saved movies
+function printSavedMovies() {
+    const savedMoviesIDs = JSON.parse(localStorage.getItem('savedMoviesIDs'));
+    if (savedMoviesIDs) {
+        handleSearchID(savedMoviesIDs);
+    }
+}
